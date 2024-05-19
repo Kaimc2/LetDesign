9db7ac0d4ae6a96953b694346f942a1446e26132
Prak Pichey <picheyprak@gmail.com>
2024-05-19 22:27:47 +0700

    feat - add fabric library

---
+++ src/lib/fabric.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+import { fabric } from "fabric"; // v5
+
+export const FabricJSCanvas = () => {
+  const canvasEl = useRef<HTMLCanvasElement>(null);
+  useEffect(() => {
+    const options = {};
+    const canvas = new fabric.Canvas(canvasEl.current, options);
+
+    // make the fabric.Canvas instance available to your app
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: "red",
+      width: 20,
+      height: 20,
+    });
+
+    canvas.add(rect);
+  }, []);
+
+  return <canvas className="border border-black" width="300" height="300" ref={canvasEl} />;
+};
