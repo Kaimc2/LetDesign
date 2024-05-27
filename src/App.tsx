6a84ef35ec2237356d87fb9c28984d965d7a317a
Prak Pichey <picheyprak@gmail.com>
2024-05-27 08:55:33 +0700

    feat - add three library

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { FabricJSCanvas } from "./lib/fabric";
+import { ThreeCanvas } from "./lib/three";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,6 +27,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <FabricJSCanvas />
+        <ThreeCanvas></ThreeCanvas>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
