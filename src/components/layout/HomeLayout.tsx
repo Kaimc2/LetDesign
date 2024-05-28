a4f025233c77395d6da1a97730e33c8e5ff24def
Prak Pichey <picheyprak@gmail.com>
2024-05-28 12:08:34 +0700

    feat - create landing page

--- src/components/layout/HomeLayout.tsx
+++ src/components/layout/HomeLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
 function HomeLayout() {
   return (
-    <div className="h-screen bg-[#f5f5f5]">
+    <div className="relative h-screen overflow-x-hidden">
+      <div className="absolute top-0 w-screen h-screen bg-[#f5f5f5] -z-10"></div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 }
