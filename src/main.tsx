ab25ccc8010e70d12fe3e21246939e377aef725b
Prak Pichey <picheyprak@gmail.com>
2024-06-13 22:00:56 +0700

    feat - add react-hot-toast for notifications

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRoutes from "./routes/AppRoutes";
 import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
   <React.StrictMode>
     <AppRoutes />
+    <Toaster />
   </React.StrictMode>
 );
