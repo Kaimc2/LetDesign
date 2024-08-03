b081a3d5fb4b7ba27b62bd2ac5f108ca33b439f3
Prak Pichey <picheyprak@gmail.com>
2024-08-03 21:41:25 +0700

    feat: add reset password function

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -49,3 +49,12 @@ export const formatDate = (date: string) => {
   const d = new Date(date);
   return d.toDateString();
 };
+
+export const evaluatePasswordStrength = (password: string) => {
+  let strength = 0;
+  if (password.length >= 8) strength += 1;
+  if (/[A-Z]/.test(password)) strength += 1;
+  if (/[0-9]/.test(password)) strength += 1;
+  if (/[^A-Za-z0-9]/.test(password)) strength += 1;
+  return strength;
+};
